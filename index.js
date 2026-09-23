a23ac98d5686f2103678dc5e01b166aa8ecbeaee
Roaa Ajlouni <roaaajlouni365@gmail.com>
2026-09-23 20:54:47 +0300

    Refactor menu display and order logic

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const menu = [
         name: "Zinger",
         price: 4,
         category: "Fast Food",
-        available: false
+        available: true
     },
     {
         name: "Pizza",
@@ -32,43 +32,43 @@ const menu = [
 ];
 function showMenu() {
     for (let i = 0; i < menu.length; i++) {
-        if (menu[i].available === false) {
-            continue;
-        }
-        console.log(
-            menu[i].name +  " - Price: " +  menu[i].price
+    document.write(
+     "<b>" + menu[i].name + "</b><br>" + "Price: " + menu[i].price + " JD<br>" + "Category: " + menu[i].category + "<br>" + "--------------------<br>"
         );
     }
 }
 showMenu();
+
+
 let selectedFood = null;
 while (selectedFood === null) {
     const foodName = prompt("Enter food name:");
     for (let i = 0; i < menu.length; i++) {
-        if (menu[i].available === false) {
-            continue;
-        }
+
         if (foodName === menu[i].name) {
             selectedFood = menu[i];
             break;
         }
     }
-    if (selectedFood === null) {alert("Food is not available. Please try again.");
+    if (selectedFood === null) {
+        alert("Food is not available. Please try again.");
     }
 }
 for (let property in selectedFood) {
 console.log(property + ": " + selectedFood[property]);
 }
-const quantity = Number(prompt("Enter quantity:"));
-const totalPrice = selectedFood.price * quantity;
+
+let quantity = Number(prompt("Enter quantity "));
+let total = selectedFood.price*quantity ;
+
 
 
 document.write("<h2>Final Order</h2>");
 document.write("Food: " + selectedFood.name + "<br>");
 document.write("Price: " + selectedFood.price + "<br>");
 document.write("Category: " + selectedFood.category + "<br>");
-document.write("Quantity: " + quantity + "<br>");
-document.write("Total Price: " + totalPrice);
+document.write("quantity : "+quantity +"<br>");
+document.write("Total :"+ total);
 
 
 //Ex : Function Task
@@ -77,36 +77,46 @@ function reverseNumber(number) {
     let result = "";
     const text = String(number);
     for (let i = text.length - 1; i >= 0; i--) {
-        result += text[i];
+        result = result + text[i];
     }
     return Number(result);
 }
 console.log(reverseNumber(532443));
-// Q2 ==>Use a loop that iterates from 0 to 15.
+
+// Q2  ==>Use a loop that iterates from 0 to 15
 for (let i = 0; i <= 15; i++) {
+
     if (i % 2 === 0) {
         console.log(i + " is even");
     } else {
         console.log(i + " is odd");
     }
 }
+
 // Q3 ==> accepts a number as input and inserts dashes (-) between each even number.
-let number = prompt("Enter a number:");
+const number = prompt("Enter a number:");
+
 let result = "";
+
 for (let i = 0; i < number.length; i++) {
-    result += number[i];
-    if ( Number(number[i]) % 2 === 0 && Number(number[i + 1]) % 2 === 0 ) {
+    result = result + number[i];
+    if (
+        Number(number[i]) % 2 === 0 && (number[i + 1]) % 2 === 0) {
         result += "-";
     }
 }
 console.log(result);
-// Q4 ==> Write a function called Agechecker that checks the user's age.
+
+// Q4==> Write a function called Agechecker that checks the user's age.
 function Agechecker(age) {
+
     if (age >= 18) {
         console.log("The user is Adult");
     } else {
         console.log("The user is Minor");
     }
 }
+
 let age = Number(prompt("Enter your age:"));
+
 Agechecker(age);
